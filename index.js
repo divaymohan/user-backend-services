680233131e65de4b2a0250b07623a6380bf7402a
Mohan <divay.mohan@teradata.com>
2021-08-10 22:48:33 -0700

    port

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(logger);
 app.use(auth);
 
  //listen at port
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port,()=>{
     console.log(`listening at port ${port}`);
 });
